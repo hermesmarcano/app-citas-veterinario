8f5f93d45691a485871afba905ef50ffae00506f
hermesmarcano <104464766+hermesmarcano@users.noreply.github.com>
2023-07-28 08:33:12 +0200

    Añadido el titulo y el formulario

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,64 @@
+
+
+function Formulario() {
+  return (
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade Pacientes y {''}
+        <span className="text-indigo-600 font-bold text-lg ">Administralos</span>
+      </p>
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 ">
+        <div className="mb-5">
+          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
+          Nombre Mascota
+          </label>
+          <input 
+          id="mascota"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+            type="text"
+            placeholder="nombre de la mascota"
+          />
+
+        </div>
+        <div className="mb-5">
+          <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">
+          Nombre Propietario
+          </label>
+          <input 
+          id="propietario"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+            type="text"
+            placeholder="nombre del propietario"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="email" className="block text-gray-700 uppercase font-bold">
+          Email
+          </label>
+          <input 
+          id="email"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+            type="email"
+            placeholder="pedronavajas@gmail.com"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">
+          Alta
+          </label>
+          <input 
+          id="alta"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+            type="date"
+            
+          />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Formulario
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,13 @@
+
+
+function ListadoPacientes() {
+  return (
+    <div className="w-1/2 lg:3/5">
+<h2>Listado Pacientes</h2>
+    </div>
+      
+    
+  )
+}
+
+export default ListadoPacientes
