13b8e7f1cee95c6c13dbe615e888532ac5e9af0f
hermesmarcano <104464766+hermesmarcano@users.noreply.github.com>
2023-07-30 18:01:46 +0200

    añadidos los estados al formulario de pacientes y una alerta en caso de no llenar los campos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,12 +2,29 @@ import {useState, useEffect} from "react"
 
 
 function Formulario() {
+ const [nombre, setNombre] = useState('')
+ const [propietario, setPropietario] = useState('')
+ const [email, setEmail] = useState('')
+ const [alta, setAlta] = useState('')
+ const [sintomas, setSintomas] = useState('')
 
-  const [nombre, setNombre] = useState('')
+ const [error, setError] = useState('')
 
-  setNombre('Piloto')
-  console.log(nombre);
 
+const handleSubmit = (e) =>{
+  e.preventDefault();
+
+//Validacion del formulario
+if ([nombre, propietario, email, alta, sintomas].includes('')) {
+  
+  setError(true)
+  return;
+} 
+setError(false)
+
+  console.log('Enviando formulario');
+}
+  
 
   return (
     <div className="md:w-1/2 lg:w-2/5">
@@ -16,16 +33,26 @@ function Formulario() {
         Añade Pacientes y {''}
         <span className="text-indigo-600 font-bold text-lg ">Administralos</span>
       </p>
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-5">
+
+      <form
+      onSubmit={handleSubmit}
+       className="bg-white shadow-md rounded-lg py-10 px-5 mb-5 mx-5">
+
+{error &&
+ <h1 className='bg-red-600 p-1 text-white text-center rounded-md uppercase font-bold'>Todos los campos son obligatorios</h1> 
+ }
+
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
-          Nombre Mascota
+          Nombre Mascota 
           </label>
           <input 
           id="mascota"
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
             type="text"
             placeholder="nombre de la mascota"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
 
         </div>
@@ -38,6 +65,8 @@ function Formulario() {
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
             type="text"
             placeholder="nombre del propietario"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
           />
         </div>
 
@@ -50,6 +79,8 @@ function Formulario() {
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
             type="email"
             placeholder="pedronavajas@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -61,6 +92,8 @@ function Formulario() {
           id="alta"
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
             type="date"
+            value={alta}
+            onChange={(e) => setAlta(e.target.value)}
             />
         </div>
 
@@ -72,6 +105,8 @@ function Formulario() {
           id="sintomas"
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
             type="textarea"
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
             
           />
         </div>
